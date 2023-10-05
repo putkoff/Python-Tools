5cac272abbef0b1a4123c52ec66927a5293557d0
putkoff <57512254+putkoff@users.noreply.github.com>
2023-10-05 04:46:18 -0400

    Update Function_Extractorfull_ast_integraed_beta.py
    ```python```
    ###Example Usage
    from abstract_script_managers import initialize_extraction
    file_path = "path/to/source/file.py"
    function_name = "function_to_extract"
    ##### the destinaton can be a full file_path, either or can be left as None of neither input and it will default to "output.py" in the current dir
    destination_dir = "destination/directory/"
    output_file_name = "output_file_name.py"
    input(initialize_extraction(file_path=file_path,function_name=function_name,destination_dir=destination_dir,output_file_name=output_file_name))

--- Function_Extractorfull_ast_integraed_beta.py
+++ Function_Extractorfull_ast_integraed_beta.py
@@ -1,11 +1,9 @@
+import os
 import ast
 import importlib.util
-import os
-from abstract_utilities.read_write_utils import read_from_file
-from abstract_utilities.string_clean import eatAll,eatInner,eatOuter
+from abstract_utilities.read_write_utils import read_from_file,write_to_file
+from abstract_utilities.string_clean import eatAll
 import builtins
-
-
 def make_list(obj):
     if not isinstance(obj,list):
         obj=[obj]
@@ -24,8 +22,62 @@ def split_it(obj,delim):
        
         return list_obj
     return make_list(obj)
+class InitialAnalyzer(ast.NodeVisitor):
+    def __init__(self,tree):
+        self.tree=tree
+        self.function_to_class_map = {}
+        self.current_class = None
+        self.import_analysis=self.analyze_imports()
+    def visit_ClassDef(self, node):
+        self.current_class = node.name
+        self.generic_visit(node)
+        self.current_class = None
 
-class FileManager:
+    def visit_FunctionDef(self, node):
+        if self.current_class:
+            self.function_to_class_map[node.name] = self.current_class
+        self.generic_visit(node)
+
+    def analyze_imports(self):
+        """
+        Analyze imports in the provided Python code using AST.
+        
+        Args:
+            code: The Python code to analyze.
+            
+        Returns:
+            A list of dictionaries containing import information.
+        """
+        # Function to get module location
+        def get_module_location(module_name):
+            spec = importlib.util.find_spec(module_name)
+            return spec.origin if spec else None
+
+        # Extract imports from the AST
+        imports = []
+        for node in ast.walk(self.tree):
+            if isinstance(node, ast.Import):
+                for n in node.names:
+                    imports.append({
+                        'type': 'import',
+                        'line': 'import ' + n.name,
+                        'source': [n.name.split('.')[0]],
+                        'alias': [n.asname] if n.asname else [],
+                        'location': get_module_location(n.name.split('.')[0])
+                    })
+            elif isinstance(node, ast.ImportFrom):
+                module = node.module
+                for n in node.names:
+                    imports.append({
+                        'type': 'from',
+                        'line': f'from {module} import {n.name}',
+                        'source': [module.split('.')[0]],
+                        'object': [n.name],
+                        'alias': [n.asname] if n.asname else [],
+                        'location': get_module_location(module.split('.')[0])
+                    })
+        return imports
+class FunctionAnalyser:
     def __init__(self,file_path):
         self.file_path = file_path
         self.directory = os.path.dirname(file_path)
@@ -33,6 +85,14 @@ class FileManager:
         self.clear_source_code = self.clear_bad_lines()
         self.lines = self.source_code.split('\n')
         self.tree = ast.parse(self.source_code)
+        self.class_methods= self.get_classes()
+        self.class_names = self.get_class_names()
+        self.function_methods = self.get_funcs()
+        self.function_names = self.get_func_names()
+        self.imports = self.get_imports()
+        self.import_names = self.get_import_names()
+        self.import_lines = self.get_import_lines()
+        self.import_ls,self.all_imps = self.extract_import_info()
     def read_file(self):
         with open(self.file_path, "r") as source:
             text = source.read()
@@ -73,75 +133,6 @@ class FileManager:
             else:
                new_doc+=char
         return new_doc
-    
-file_mgr = FileManager(file_path = "/home/joben_joe/Documents/modules/abstract_py/src/abstract_py/test_scripts/limit_test.py")
-class InitialAnalyzer(ast.NodeVisitor):
-    def __init__(self):
-        self.function_to_class_map = {}
-        self.current_class = None
-        self.import_analysis=self.analyze_imports()
-    def visit_ClassDef(self, node):
-        self.current_class = node.name
-        self.generic_visit(node)
-        self.current_class = None
-
-    def visit_FunctionDef(self, node):
-        if self.current_class:
-            self.function_to_class_map[node.name] = self.current_class
-        self.generic_visit(node)
-
-    def analyze_imports(code: str):
-        """
-        Analyze imports in the provided Python code using AST.
-        
-        Args:
-            code: The Python code to analyze.
-            
-        Returns:
-            A list of dictionaries containing import information.
-        """
-        # Function to get module location
-        def get_module_location(module_name):
-            spec = importlib.util.find_spec(module_name)
-            return spec.origin if spec else None
-
-        # Extract imports from the AST
-        imports = []
-        for node in ast.walk(file_mgr.tree):
-            if isinstance(node, ast.Import):
-                for n in node.names:
-                    imports.append({
-                        'type': 'import',
-                        'line': 'import ' + n.name,
-                        'source': [n.name.split('.')[0]],
-                        'alias': [n.asname] if n.asname else [],
-                        'location': get_module_location(n.name.split('.')[0])
-                    })
-            elif isinstance(node, ast.ImportFrom):
-                module = node.module
-                for n in node.names:
-                    imports.append({
-                        'type': 'from',
-                        'line': f'from {module} import {n.name}',
-                        'source': [module.split('.')[0]],
-                        'object': [n.name],
-                        'alias': [n.asname] if n.asname else [],
-                        'location': get_module_location(module.split('.')[0])
-                    })
-        return imports
-class FunctionAnalyser:
-    def __init__(self):
-        self.source_code= file_mgr.source_code
-        self.tree = file_mgr.tree
-        self.lines= file_mgr.lines
-        self.class_methods= self.get_classes()
-        self.class_names = self.get_class_names()
-        self.function_methods = self.get_funcs()
-        self.function_names = self.get_func_names()
-        self.imports = self.get_imports()
-        self.import_names = self.get_import_names()
-        self.import_lines = self.get_import_lines()
-        self.import_ls,self.all_imps = self.extract_import_info()
     def get_funcs(self):
         return [func for func in ast.walk(self.tree) if isinstance(func, ast.FunctionDef)]
     def get_func_names(self):
@@ -161,7 +152,7 @@ class FunctionAnalyser:
         template_js = {"from":"source","import":{"import":"source","from":"object"},"as":"alias"}
         import_ls=[]
         all_imps=[]
-        for line in file_mgr.lines:
+        for line in self.lines:
             line_lst=line.split(' ')
             if line_lst[0] in ["from","import"]:
                 
@@ -316,104 +307,119 @@ class ScriptAnalyzer(ast.NodeVisitor):
             else:
                 self.functions.add(node.func.attr)
         self.generic_visit(node)
-
-
-
-
-initial_analyzer = InitialAnalyzer()
-def map_functions_to_classes(script):
-    tree = ast.parse(script)
-    
-    initial_analyzer.visit(tree)
-    return initial_analyzer.function_to_class_map
-function_to_class_map=map_functions_to_classes(file_mgr.source_code)
-analyzer = ScriptAnalyzer(function_to_class_map)
-def analyze_script(script):
-    tree = ast.parse(script)
-    analyzer.visit(tree)
-    return analyzer.classes, analyzer.functions, analyzer.imports
-def set_parents(node, parent=None):
-    for child in ast.iter_child_nodes(node):
-        child.parent = parent
-        set_parents(child, child)
-def integrated_analysis(code):
-    # Get all the imports from the code
-    imports = initial_analyzer.import_analysis
-    # Create a mapping for easier reference
-    import_mapping = {}
-    for entry in imports:
-        if 'alias' in entry and entry['alias']:
-            import_mapping[entry['alias'][0]] = entry
-        else:
-            for obj in entry.get('object', []):
-                import_mapping[obj] = entry
-            for src in entry['source']:
-                import_mapping[src] = entry
-    # Analyze the code using the previous function
-    classes, functions, imports_used = analyze_script(code)
-
-    # Refine the functions set to provide more detailed information
-    refined_functions = set()
-    for func in functions:
-        if func in import_mapping:
-            module_name = import_mapping[func]['source'][0]
-            refined_functions.add(f"{module_name}.{func}")
-        else:
-            refined_functions.add(func)
-
-    return classes, refined_functions, imports_used
-def build_the_import(import_names):
-    all_imports_ls,all_imports = FunctionAnalyser().extract_import_info()
-    imports_needed=[]
-    for obj in import_names:
-        spl_imp = obj.split('.')
-        if len(spl_imp)>1 and isinstance(spl_imp,list):
-            obj=spl_imp[-1]
-        for import_js in all_imports_ls:
-            import_phrase = import_js["type"]+' '+(',').join(import_js["source"])
-            if "alias" in import_js:
-                 if obj in import_js["alias"]:
-                    if "object" in import_js:
-                        import_phrase=import_phrase+' '+(',').join(import_js["object"])
-                    import_phrase=import_phrase+' as '+obj
-                    if import_phrase not in imports_needed:
-                        imports_needed.append(import_phrase)
-            elif "object" in import_js:
-                if obj in import_js["object"]:
-                    import_phrase=import_phrase+' import '+obj
-                    if import_phrase not in imports_needed:
-                        imports_needed.append(import_phrase)
+class ExtractionManager:
+    def __init__(self,file_path,functions_list=[]):
+        self.function_manager = FunctionAnalyser(file_path = file_path)
+        self.initial_analyzer = InitialAnalyzer(self.function_manager.tree)
+        self.all_imports = self.initial_analyzer.import_analysis
+        self.function_to_class_map=self.map_functions_to_classes(self.initial_analyzer,self.function_manager.source_code)
+        self.analyzer = ScriptAnalyzer(self.function_to_class_map)
+        self.functions_list = functions_list
+        self.output_file_contents = None
+    def map_functions_to_classes(self,initial_analyzer,script):
+        tree = ast.parse(script)
+        initial_analyzer.visit(tree)
+        return initial_analyzer.function_to_class_map
+    def analyze_script(self,script=None):
+        tree = ast.parse(script) or self.function_manager.tree
+        self.analyzer.visit(tree)
+        return self.analyzer.classes, self.analyzer.functions, self.analyzer.imports
+    def integrated_analysis(self,code=None):
+        # Create a mapping for easier reference
+        imports = self.initial_analyzer.import_analysis
+        code = code or self.function_manager.source_code
+        import_mapping = {}
+        for entry in imports:
+            if 'alias' in entry and entry['alias']:
+                import_mapping[entry['alias'][0]] = entry
             else:
-                if obj in import_js["source"]:
-                    if import_phrase not in imports_needed:
-                        imports_needed.append(import_phrase)
-    return imports_needed
+                for obj in entry.get('object', []):
+                    import_mapping[obj] = entry
+                for src in entry['source']:
+                    import_mapping[src] = entry
+        # Analyze the code using the previous function
+        classes, functions, imports_used = self.analyze_script(code)
 
+        # Refine the functions set to provide more detailed information
+        refined_functions = set()
+        for func in functions:
+            if func in import_mapping:
+                module_name = import_mapping[func]['source'][0]
+                refined_functions.add(f"{module_name}.{func}")
+            else:
+                refined_functions.add(func)
 
-function_name = "fetch_price_from_coingecko"
-# Given lists/sets and the function name
-function_definition = FunctionAnalyser().get_func_code(function_name)
-analyzer.classes_geko, analyzer.functions_geko, analyzer.imports_geko =integrated_analysis(function_definition)
-analyzer.classes, analyzer.functions, analyzer.imports =integrated_analysis(file_mgr.source_code)
-function_attributions = analyzer.functions  # and so on
-# 1. Extract imports
-required_imports = []
-for imp in analyzer.imports:
-    # This logic assumes that you've got some mapping which provides the actual import lines
-    required_imports+=build_the_import([imp]) # Using the function you've provided
+        return classes, refined_functions, imports_used
+    def build_the_import(self,import_names):
+        all_imports_ls,all_imports = self.function_manager.extract_import_info()
+        imports_needed=[]
+        for obj in import_names:
+            spl_imp = obj.split('.')
+            if len(spl_imp)>1 and isinstance(spl_imp,list):
+                obj=spl_imp[-1]
+            for import_js in all_imports_ls:
+                import_phrase = import_js["type"]+' '+(',').join(import_js["source"])
+                if "alias" in import_js:
+                     if obj in import_js["alias"]:
+                        if "object" in import_js:
+                            import_phrase=import_phrase+' '+(',').join(import_js["object"])
+                        import_phrase=import_phrase+' as '+obj
+                        if import_phrase not in imports_needed:
+                            imports_needed.append(import_phrase)
+                elif "object" in import_js:
+                    if obj in import_js["object"]:
+                        import_phrase=import_phrase+' import '+obj
+                        if import_phrase not in imports_needed:
+                            imports_needed.append(import_phrase)
+                else:
+                    if obj in import_js["source"]:
+                        if import_phrase not in imports_needed:
+                            imports_needed.append(import_phrase)
+        return imports_needed
+    def finalize_extraction(self,function_name):
+        # Given lists/sets and the function name
+        function_definition_standalone = self.function_manager.get_func_code(function_name)
+        self.analyzer.classes_stand_alone, self.analyzer.functions_stand_alone, self.analyzer.imports_stand_alone =self.integrated_analysis(function_definition_standalone)
+        
+        self.analyzer.classes, self.analyzer.functions, self.analyzer.imports =self.integrated_analysis(self.function_manager.source_code)
+        # 1. Extract imports
+        required_imports = []
+        for imp in self.analyzer.imports:
+            # This logic assumes that you've got some mapping which provides the actual import lines
+            required_imports+=self.build_the_import([imp]) 
+            
+        # 2. Extract function definitions
+        # Assuming you have a mechanism similar to FunctionAnalyser().get_func_code
+        function_definitions = [self.function_manager.get_func_code(func) for func in self.analyzer.functions_stand_alone]
 
-input(required_imports)
-# 2. Extract function definitions
-# Assuming you have a mechanism similar to FunctionAnalyser().get_func_code
-function_definitions = [FunctionAnalyser().get_func_code(func) for func in analyzer.functions_geko]
+        # 3. Extract class definitions
+        # Assuming you have a mechanism to extract full class definitions
+        class_definitions = [self.function_manager.get_func_code(cls) for cls in self.analyzer.classes_stand_alone]
 
-# 3. Extract class definitions
-# Assuming you have a mechanism to extract full class definitions
-class_definitions = [FunctionAnalyser().get_func_code(cls) for cls in analyzer.classes_geko]
-#input(required_imports)
-# 4. Combine to generate standalone script
-standalone_script = '\n\n'.join(required_imports+ class_definitions + function_definitions + [function_definition])
+        # 4. Combine to generate standalone script
+        self.output_file_contents = '\n\n'.join(required_imports+ class_definitions + function_definitions)
+    def check_destination(self,destination, output_file_name):
+        # If both are None, set a default path in the current directory
+        if not destination and not output_file_name:
+            return os.path.join(os.getcwd(), 'output.py')
 
-#standalone_code = create_standalone_script(function_name, initial_analyzer.import_analysis, function_attributions)
-print("standalone output:\n",standalone_script)
+        # If only a file name is provided, join it with the current directory
+        if not destination:
+            return os.path.join(os.getcwd(), output_file_name)
 
+        # If only a directory (destination) is provided, check if it's an actual directory
+        if not output_file_name:
+            if os.path.isdir(destination):
+                return os.path.join(destination, 'output.py')
+            else:
+                return destination
+
+        # If both are provided, join them
+        return os.path.join(destination, output_file_name)
+    def save_the_file(self,destination=None,output_file_name=None):
+        write_to_file(file_path=self.check_destination(destination=destination,output_file_name=output_file_name),contents=self.output_file_contents)
+def initialize_extraction(file_path,function_name,destination=None,output_file_name=None):
+    extraction_manager = ExtractionManager(file_path)
+    extraction_manager.finalize_extraction(function_name=function_name)
+    extraction_manager.save_the_file(destination=destination,output_file_name=output_file_name)
+    return extraction_manager.output_file_contents
